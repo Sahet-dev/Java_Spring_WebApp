2d45aac983633db91a47a3bd55f9de7c745eab4e
Sahetmyrat <kakajansahy@gmail.com>
2024-11-27 10:11:08 +0200

    version files

---
+++ src/main/java/com/sahet/simplewebapp/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.sahet.simplewebapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String greet(){
+        return "Welcome to Simple Web App";
+    }
+}
